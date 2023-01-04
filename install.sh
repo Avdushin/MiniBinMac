27181ba37fac9d34e9b3b16c786f7b80473fe182
Avdushin <avdushinbeaver1@gmail.com>
2023-01-05 00:44:56 +0300

    Updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 echo "Install app..."
 sudo cp -rf bin/minibinmac /opt/local/bin
+sudo cp -rf bin/minibinmac.sh /opt/local/bin
+sudo cp -rf bin/minibinmac.sh /usr/local/bin
 sudo cp -rf bin/minibinmac /usr/local/bin
 echo "Done..."
 
